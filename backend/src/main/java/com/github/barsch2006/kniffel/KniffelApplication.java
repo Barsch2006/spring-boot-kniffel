7c7bf782c88d2e62309e78d5776632eb76aa27a6
Barsch2006 <christian0511@gmx.de>
2024-01-07 15:29:26 +0100

    init/chapter1

---
+++ backend/src/main/java/com/github/barsch2006/kniffel/KniffelApplication.java
@@ -0,0 +1,13 @@
+package com.github.barsch2006.kniffel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KniffelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KniffelApplication.class, args);
+	}
+
+}
